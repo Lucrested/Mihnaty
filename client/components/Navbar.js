56203067a95b4edbcb4615c96f6b2471687f74ce
Ghali Joudary <ghalijoudary03@gmail.com>
2023-11-07 18:23:05 +0100

    Started working on the navbar

---
+++ client/components/Navbar.js
@@ -0,0 +1,54 @@
+import * as React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { BottomSheet, Button } from "react-native-elements";
+// import * as Icons from "react-native-heroicons/solid";
+
+const onTabPress = () => {};
+
+export default function Navbar() {
+  const tailwind = useTailwind();
+  return (
+    <View style={navBarStyles.container}>
+      <TouchableOpacity onPress={() => onTabPress(0)}>
+        <Text>edgihwugb</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => onTabPress(1)}>
+        <Text>edgihwugb</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => onTabPress(2)}>
+        <Text>edgihwugb</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={navBarStyles.Button}
+        onPress={() => onTabPress(4)}
+      >
+        {/* <Icons.SparklesIcon /> */}
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const navBarStyles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    bottom: 25,
+    right: 0,
+    left: 0,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    // flex: 1,
+    // justifyContent: "space-between",
+    display: "inline",
+  },
+  Button: {
+    padding: 10,
+    borderRadius: 5,
+    width: 100,
+    backgroundColor: "rgb(255,53,33)",
+  },
+  ButtonText: {
+    alignItems: "center",
+    padding: 10,
+  },
+});
---
+++ client/pages/CategoryPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import { View } from "react-native";
+
+export default function CategoryPage() {
+  return (
+    <View style={{ flex: 1 }}>
+      <Navbar />
+    </View>
+  );
+}
