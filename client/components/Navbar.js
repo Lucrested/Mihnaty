94b4d69ffe1b39c7056f2beaaccbb2e580bdd5da
Ghali Joudary <ghalijoudary03@gmail.com>
2023-11-08 21:35:24 +0100

    start work on navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,28 +1,48 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
 import * as React from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { BottomSheet, Button } from "react-native-elements";
+import { BottomSheet, Button, Icon } from "react-native-elements";
 
-const onTabPress = () => {};
+const Tab = createBottomTabNavigatoravigator();
 
-export default function Navbar() {
+const CustomTabIcon = ({ source, color, size }) => (
+  <Image
+    source={source}
+    style={{ tintColor: color, width: size, height: size }}
+  />
+);
+
+const Navigation = () => {
   return (
-    <View style={navBarStyles.container}>
-      <TouchableOpacity onPress={() => onTabPress(0)}>
-        <Text>edgihwugb</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => onTabPress(1)}>
-        <Text>edgihwugb</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => onTabPress(2)}>
-        <Text>edgihwugb</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={navBarStyles.Button}
-        onPress={() => onTabPress(4)}
-      ></TouchableOpacity>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Home"
+        tabBarOptions={{
+          showLabel: false, // Hide tab labels
+        }}
+      >
+        {[
+          { name: "Home", icon: require() },
+          { name: "Calendar", icon: require("/icons/calendar.png") },
+          { name: "Search", icon: require("/icons/search.png") },
+          { name: "AccountSettings", icon: require("/icons/account.png") },
+        ].map((tab) => (
+          <Tab.Screen
+            key={tab.name}
+            name={tab.name}
+            // component={tab.component} // Replace with the actual component for this tab
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <CustomTabIcon source={tab.icon} color={color} size={size} />
+              ),
+            }}
+          />
+        ))}
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
 const navBarStyles = StyleSheet.create({
   container: {
