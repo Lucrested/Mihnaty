9c1031c27c95bc6014e44aa36491ccee13c36b69
Ghali Joudary <ghalijoudary03@gmail.com>
2023-11-30 13:48:15 +0100

    not important

--- client/components/CategoryCard.js
+++ client/components/CategoryCard.js
@@ -21,6 +21,47 @@ const CategoryCard = ({ category }) => {
   );
 };
 
+const OptionItem = ({ bgColor, icon, label, onPress }) => {
+  return (
+    <TouchableOpacity
+      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+      onPress={onPress}
+    >
+      <View style={[styles.shadow, { width: 60, height: 60 }]}>
+        <LinearGradient
+          style={[
+            {
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: 15,
+              backgroundColor: "red",
+            },
+          ]}
+          colors={bgColor}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 0, y: 1 }}
+        >
+          <Image
+            source={icon}
+            resizeMode="cover"
+            style={{
+              tintColor: COLORS.white,
+              width: 30,
+              height: 30,
+            }}
+          />
+        </LinearGradient>
+      </View>
+      <Text
+        style={{ marginTop: SIZES.base, color: COLORS.gray, ...FONTS.body3 }}
+      >
+        {label}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
 const categoryStyles = StyleSheet.create({
   cardContainer: {
     borderRadius: 10,
